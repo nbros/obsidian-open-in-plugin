3f8670f07bfb71fc6d87abb6d6de4a4adc67e08d
nbros <nicolas.bros@gmail.com>
2025-04-29 13:27:42 +0200

    "open-in" plugin : registers 2 commands to open the current vault root directory either in Windows Explorer or Windows Terminal.

--- main.ts
+++ main.ts
@@ -1,134 +1,63 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, FileSystemAdapter } from 'obsidian';
+import { exec } from "child_process"; // Import exec from Node.js
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class OpenInPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'open-in-terminal',
+			name: 'Open the current vault in a Windows Terminal',
+			callback: () => this.openTerminal(),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+			id: 'open-in-explorer',
+			name: 'Open the current vault in Windows Explorer',
+			callback: () => this.openExplorer(),
+		});
+	}
+
+	onunload() { }
+
+	openExplorer() {
+		// Command for opening in Windows Explorer
+		this.commandOnVault("start explorer");
+	}
+
+	openTerminal() {
+		// Command for opening in Windows Terminal
+		// wt.exe -d "directory" opens a new tab/window in the specified directory
+		this.commandOnVault("wt.exe -d");
+	}
+
+	commandOnVault(commandLine: string) {
+		// Check if the adapter is a FileSystemAdapter (desktop app)
+		if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
+			new Notice("This command is only available on the desktop version of Obsidian.");
+			return;
+		}
+
+		// Get the vault's absolute base path
+		const vaultPath = this.app.vault.adapter.getBasePath();
+
+		// build command (wrap the path in quotes to handle spaces)
+		const command = `${commandLine} "${vaultPath}"`;
+
+		console.debug(`Executing command: ${command}`);
+
+		// Execute the command
+		exec(command, (error, stdout, stderr) => {
+			if (error) {
+				console.error(`Failed to open Windows Terminal: (RC ${error.code}) ${error.message}`);
+				new Notice(`Failed to open Windows Terminal: (RC ${error.code}) ${error.message}`,);
+				return;
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			if (stderr) {
+				console.error(`stderr: ${stderr}`);
 			}
+			if (stdout) {
+				console.debug(`stdout: ${stdout}`);
+			}
+
+			new Notice(`Opened in Windows Terminal.`);
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
